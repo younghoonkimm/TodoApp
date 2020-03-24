e9c348ca8a4d0f3eaa2d96ea8f6c8f611ff4c36b
younghoon <choosey_@naver.com>
2020-03-24 22:24:47 +0900

    Updates

---
+++ precache-manifest.6766e5ffad0997bdd69fadd5ae22618a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d6b6d9cf8411f9996d1dfa6cc72194",
+    "url": "/younghoonKimm/TodoApp.git/index.html"
+  },
+  {
+    "revision": "196c07872a955a664157",
+    "url": "/younghoonKimm/TodoApp.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "768423e7904d7fa1329f",
+    "url": "/younghoonKimm/TodoApp.git/static/js/2.bf72f523.chunk.js"
+  },
+  {
+    "revision": "058c14a89d808fe8a38664b65e903dd7",
+    "url": "/younghoonKimm/TodoApp.git/static/js/2.bf72f523.chunk.js.LICENSE"
+  },
+  {
+    "revision": "196c07872a955a664157",
+    "url": "/younghoonKimm/TodoApp.git/static/js/main.4c55be1a.chunk.js"
+  },
+  {
+    "revision": "24e2b12486956ce43cf5",
+    "url": "/younghoonKimm/TodoApp.git/static/js/runtime-main.228214b2.js"
+  }
+]);
